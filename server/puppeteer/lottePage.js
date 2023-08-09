7628af174865366f3719bc0cbd1cd588ca26aeff
dona6382 <46666529+dona6382@users.noreply.github.com>
2023-08-09 09:01:20 +0900

    [lotto] 페이지 url 변경 및 옵션 클릭 변경

--- server/puppeteer/lottePage.js
+++ server/puppeteer/lottePage.js
@@ -30,8 +30,9 @@ async function pageOpen(){
 
                 try {
                     await page.goto('https://dhlottery.co.kr/user.do?method=login&returnUrl=');   // 로그인 페이지
+                    // await page.waitForNavigation();
 
-                    const detailPage = cheerio.load(await page.content());
+                    // const detailPage = cheerio.load(await page.content());
 
                     await page.click('#userId');
                     await page.type('#userId', 'kim63826382', {delay:50});
@@ -42,19 +43,34 @@ async function pageOpen(){
 
                     await sleep(2);
 
-                    await page.goto('https://el.dhlottery.co.kr/game/TotalGame.jsp?LottoId=LO40');  // 로또 페이지 이동
+                    await page.goto('https://ol.dhlottery.co.kr/olotto/game/game645.do');  // 나눔 로또 direct Page
+                    // await page.waitForNavigation();
 
-                    //TODO 자동번호 클릭부터 이슈
+                    const sdsd = await page.content();
+                    const detailPage = cheerio.load(await page.content());
+
+
                     await page.click('ul#tabWay2Buy > li > a#num2')         // num2 == 자동구매
-                    await page.click('#amoundApply > option:nth-child(5)')  // 적용수량 설정
-                    await page.click('#btnSelectNum');                      // 확인
-                    await page.click('#btnBuy');                            // 구매하기
 
-                    await sleep(2);
+                    const selectElement = await page.$('select#amoundApply');
 
-                    await page.click('#popupLayerConfirm > div > div.btns > input:nth-child(1)');   // 팝업 구매버튼
+                    if(selectElement){
+                        await selectElement.click();
 
+                        const optionValue = 'option:nth-child(5)';
 
+                        //TODO 옵션 수량 확인
+                        await page.select('select#amoundApply', optionValue);
+                        console.log(`옵션 ${optionValue}가 선택되었습니다.`);
+
+                        await page.click('#amoundApply > option:nth-child(5)')  // 적용수량 설정
+                        await page.click('#btnSelectNum');                      // 확인
+                        await page.click('#btnBuy');                            // 구매하기
+
+                        await sleep(2);
+
+                        await page.click('#popupLayerConfirm > div > div.btns > input:nth-child(1)');   // 팝업 구매버튼
+                    }
 
                 } catch (e) {
                     console.log('페이지 로딩 실패')
