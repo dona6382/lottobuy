45f4fdb13a98936dbf7c3bb6929b4bfee764aff7
dona6382 <46666529+dona6382@users.noreply.github.com>
2023-08-16 09:25:06 +0900

    [lotto] 구매 flow추가

--- server/puppeteer/lottePage.js
+++ server/puppeteer/lottePage.js
@@ -15,7 +15,7 @@ async function pageOpen(){
 
             const browser = await puppeteer.launch({
                 executablePath: chromiumExecutablePath,
-                // headless: false,
+                // headless: true,
                 headless: false,
                 defaultViewport: {
                     width: 1280,
@@ -34,9 +34,9 @@ async function pageOpen(){
 
                 try {
                     await page.goto('https://dhlottery.co.kr/user.do?method=login&returnUrl=');   // 로그인 페이지
-                    // await page.waitForNavigation();
 
-                    // const detailPage = cheerio.load(await page.content());
+
+
                     const userInfo = await user.getUser();
                     const {userId} = userInfo;
                     const {userPassword} = userInfo;
@@ -57,34 +57,28 @@ async function pageOpen(){
 
                     await page.click('ul#tabWay2Buy > li > a#num2')         // num2 == 자동구매
 
-                    const selectElement = await page.$('select#amoundApply');
+
+                    // const selectElement = await page.$('select#amoundApply');
+                    //TODO 입력받은 수량 만큼 values 변경 가능
+                    const selectElement = await page.select('select#amoundApply', '5');
+
 
                     if(selectElement){
-                        await selectElement.click();
-
-                        const optionValue = 'option:nth-child(5)';
-
-                        await page.select('select#amoundApply', optionValue);
-                        console.log(`옵션 ${optionValue}가 선택되었습니다.`);
-
-                        //TODO 옵션 수량 확인
-                        await page.click('#amoundApply > option:nth-child(5)')  // 적용수량 설정
                         await page.click('#btnSelectNum');                      // 확인
                         await page.click('#btnBuy');                            // 구매하기
 
                         await sleep(2);
 
                         await page.click('#popupLayerConfirm > div > div.btns > input:nth-child(1)');   // 팝업 구매버튼
+
+                        const detailPage = cheerio.load(await page.content());
+                        const notiMessage = await detailPage('#popupLayerAlert > div > div.noti > span').text();
+
                     }
 
                 } catch (e) {
                     console.log('페이지 로딩 실패')
                 }
-
-                await sleep(5);
-
-                const detailPage = cheerio.load(await page.content());
-
 
             } catch (error) {
                 console.error('오류 발생:', error);
@@ -102,6 +96,6 @@ const main = async () =>{
 
 
 
-// main();
+main();
 
 module.exports = {pageOpen};
